bf5f15f1bbc69782f5825547e9449299004a7719
sujalpathak05 <sharmapharmacy111@gmail.com>
2026-05-29 14:36:45 +0530

    Fix JWT_EXPIRE hardcode to avoid env variable parsing issue

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (userId, role) => {
   return jwt.sign(
     { id: userId, role },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRE || '7d' }
+    { expiresIn: '7d' }
   );
 };
 
